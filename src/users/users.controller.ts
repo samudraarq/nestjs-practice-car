8872b3b90d1c47ec1d22d11bb901310bf14525d9
Samudra Faris Arqam <samudrafaris@gmail.com>
2024-03-14 10:34:05 +0700

    create interceptors

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,8 +12,11 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
